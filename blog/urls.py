e99a1a1b4ac7d9ae0392c5a0cfd9bdce1f0d66e2
wjlhhh <15101003835@163.com>
2018-06-28 18:08:27 +0800

    fff

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 app_name='blog'
 urlpatterns = [
     #post views
     path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug>/',
-         views.post_detail,name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<str:post>/',views.post_detail,name='post_detail'),
 ]
